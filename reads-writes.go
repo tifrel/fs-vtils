44a9eadbba6ee33e27770ef91260e9b41e4a0f8a
tillyboy <till.friesewinkel@googlemail.com>
2018-06-06 15:09:28 +0200

    Added Appends

--- reads-writes.go
+++ reads-writes.go
@@ -1,8 +1,11 @@
 package fsv
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"os"
+)
 
-// --------------------------------- reads  --------------------------------- //
+// --------------------------------- reads ---------------------------------- //
 
 // ReadString reads the contents of the file at p and returns them as a string.
 func (p Path) ReadString() (string, error) {
@@ -21,7 +24,7 @@ func (p Path) ReadBytes() ([]byte, error) {
 	return ioutil.ReadFile(string(p))
 }
 
-// -------------------------------- writes  --------------------------------- //
+// --------------------------------- writes --------------------------------- //
 
 // WriteString (over)writes the contents of the file located at p with a string.
 func (p Path) WriteString(contents string) error {
@@ -35,7 +38,6 @@ func (p Path) WriteLines(lines []string) error {
 
 // WriteBytes (over)writes the contents of the file located at p with a []byte.
 func (p Path) WriteBytes(bytes []byte) error {
-	// TODO: verify that the contents are overwritten
 	nfo, err := p.Info()
 	if err != nil {
 		return err
@@ -44,4 +46,26 @@ func (p Path) WriteBytes(bytes []byte) error {
 	return ioutil.WriteFile(string(p), bytes, nfo.Mode())
 }
 
-// TODO: AppendString, AppendLines, AppendBytes
+// -------------------------------- appends --------------------------------- //
+
+// AppendString appends its argument to a file
+func (p Path) AppendString(contents string) error {
+	return p.WriteBytes([]byte(contents))
+}
+
+// AppendLines appends its argument to a file
+func (p Path) AppendLines(lines []string) error {
+	return p.WriteString(joinWith(lines, "\n"))
+}
+
+// AppendBytes appends its argument to a file
+func (p Path) AppendBytes(bytes []byte) error {
+	f, err := os.OpenFile(string(p), os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(bytes)
+	return err
+}
